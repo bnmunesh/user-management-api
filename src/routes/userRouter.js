afa9da2e062b6d9a785c871949a70e98ce7c6be7
bnmunesh <muneshkumarbn020@gmail.com>
2024-09-28 14:50:11 +0530

    added jwt authentication to endpoints

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
+const authenticateJWT = require('../middleware/jwtAuthenticationFilter.js');
 
 const router = express.Router();
 
-router.post('/users', userController.createUser);
+// Public route (no authentication required)
 router.post('/login', userController.loginUser);
-router.get('/users', userController.getAllUsers);
-router.get('/users/:userId', userController.getUserById);
-router.put('/users/:userId', userController.updateUser);
-router.delete('/users/:userId', userController.softDeleteUser);
-router.delete('/users/:userId/force', userController.forceDeleteUser);
+router.post('/users', userController.createUser);
+
+// Protected routes (authentication required)
+router.get('/users', authenticateJWT, userController.getAllUsers);
+router.get('/users/:userId', authenticateJWT, userController.getUserById);
+router.put('/users/:userId', authenticateJWT, userController.updateUser);
+router.delete('/users/:userId', authenticateJWT, userController.softDeleteUser);
+router.delete('/users/:userId/force', authenticateJWT, userController.forceDeleteUser);
 
 module.exports = router;
